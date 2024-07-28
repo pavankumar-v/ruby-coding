656e61cc5d10742c3bf119c22229342068be1f28
Pavan Kumar V <66193049+pavankumar-v@users.noreply.github.com>
2024-07-28 17:55:26 +0000

    chore: count digits

--- DSA/maths.rb
+++ DSA/maths.rb
@@ -16,3 +16,20 @@ end
 
 # puts count_digits(12345)
 # puts count_digits(1234345)
+
+# REVERSE A NUMBER
+def reverse_number(num)
+    ans = 0
+
+    while num > 0
+        last_digit = num % 10
+        ans += last_digit * 10 
+
+        num = num / 10
+    end
+
+
+    ans
+end
+
+puts reverse_number(12345)
