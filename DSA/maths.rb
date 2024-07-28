59fa4aaa3d355a828ffcc024ba7591f8f32e1c11
Pavan Kumar V <66193049+pavankumar-v@users.noreply.github.com>
2024-07-28 18:01:50 +0000

    chore: check if palindrom

--- DSA/maths.rb
+++ DSA/maths.rb
@@ -32,9 +32,20 @@ def reverse_number(num)
     ans
 end
 
-puts reverse_number(12345)
-puts reverse_number(3912)
-puts reverse_number(1)
-puts reverse_number(12)
-puts reverse_number(1212)
-puts reverse_number(1221) # palindrome
+# puts reverse_number(12345)
+# puts reverse_number(3912)
+# puts reverse_number(1)
+# puts reverse_number(12)
+# puts reverse_number(1212)
+# puts reverse_number(1221) # palindrome
+
+# PALINDROME
+def check_palindrome(num)
+    n = num
+    num == reverse_number(n)
+end
+
+puts check_palindrome(1221)
+puts check_palindrome(1212)
+puts check_palindrome(1234)
+puts check_palindrome(1222221)
