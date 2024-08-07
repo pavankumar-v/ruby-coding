54ed1345b018272d71bedfb5ade73152913dbb25
Pavan Kumar V <66193049+pavankumar-v@users.noreply.github.com>
2024-08-07 05:11:00 +0000

    chore: adapter pattern add planning

--- design_patterns/adapter/adapter.rb
+++ design_patterns/adapter/adapter.rb
@@ -31,9 +31,10 @@ Let's list down configurations for each
 
 =begin
 Functional Requirements
-1. Once Db adataper should give us instance of client
-2. Query passed should show execute and output based on query
-3. Display data in table format
+1. The adapter should support [MySQL, Postgresql & sqlite]
+2. Once Db adataper should give us instance of client
+3. Query passed should show execute and output based on query
+4. Display data in table format
 
 Non Functional Requirements
 1. Add validation to configuration
@@ -41,5 +42,15 @@ Non Functional Requirements
 2. Shrow Error on Database Query Execution
 =end
 
-
- 
+# Planning
+=begin
+We Require 5 Classes based on our requirements
+1. Interface Class (Abstract Pattern)
+    -> This is more like making the implementation of classes strict
+    -> it any class that will inherit, should implement those methods or else, it throws error
+2. Adapter Class
+    -> creates instance of adaptee (yes adapteee!)
+    -> creates connection object
+3. MySQL, Postgresql, sqlite Classes
+    -> implements basic methods like query and connect
+=end
